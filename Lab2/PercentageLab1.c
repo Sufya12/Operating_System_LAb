9dd6b76513fd81d7ce327413e0efa1b3fbc4c0e8
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-12-11 10:37:32 +0500

    Add files via upload

---
+++ Lab2/PercentageLab1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	float number = 1000;
+	float one_perce = number/100;
+	float twentyfive_prece = one_perce *25;
+	printf("The 25 percentage is %f ",twentyfive_prece);
+	return 0;
+}
