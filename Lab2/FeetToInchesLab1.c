176642dc267b8cb5c741371bc440592275e25e8c
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-12-11 11:06:07 +0500

    Add files via upload

---
+++ Lab2/FeetToInchesLab1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	//Create the Variable of Feet And Value
+	float feet = 124;
+	//Create the Variable of Inches  And Apply Formula of Feet into Inches 
+	float inch = feet * 12;
+	//Print The Feet Value 
+	printf("The Feet Value is = %f \n",feet );
+	//Print The Inches  Value
+	printf("The Feet Value Convert into Inches Value is =  %f",inch); 
+}
