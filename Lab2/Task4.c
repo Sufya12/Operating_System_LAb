a1de82ac8406462e77e1fe9eed79aec06346888a
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-12-11 10:54:43 +0500

    Add files via upload

---
+++ Lab2/Task4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+
+float f = 12.3;
+float c = (f - 32) / (9/5);
+printf("The of  Fahrenheit Temperature  = %f  \n",f);
+printf("The of  Fahrenheit  Convert Into  Celsius Temperature  = %f",c);
+return 0;
+}
